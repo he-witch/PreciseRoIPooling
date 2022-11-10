0422fda1fb8e70887e5c3bc7546f3c3216dfdf0c
he-witch <doganay.demir.tr@gmail.com>
2022-11-10 10:46:46 +0300

    Add compilation support for pytorch >= 1.11.x

--- pytorch/prroi_pool/src/prroi_pooling_gpu.c
+++ pytorch/prroi_pool/src/prroi_pooling_gpu.c
@@ -13,8 +13,9 @@
 
 #include <ATen/ATen.h>
 #include <ATen/cuda/CUDAContext.h>
+#include <ATen/cuda/CUDAEvent.h>
 
-#include <THC/THC.h>
+//#include <THC/THC.h>
 
 #include "prroi_pooling_gpu_impl.cuh"
 
@@ -28,7 +29,7 @@ at::Tensor prroi_pooling_forward_cuda(const at::Tensor &features, const at::Tens
     auto output = at::zeros({nr_rois, nr_channels, pooled_height, pooled_width}, features.options());
 
     if (output.numel() == 0) {
-        THCudaCheck(cudaGetLastError());
+        AT_CUDA_CHECK(cudaGetLastError());
         return output;
     }
 
@@ -39,7 +40,7 @@ at::Tensor prroi_pooling_forward_cuda(const at::Tensor &features, const at::Tens
         top_count
     );
 
-    THCudaCheck(cudaGetLastError());
+    AT_CUDA_CHECK(cudaGetLastError());
     return output;
 }
 
@@ -58,7 +59,7 @@ at::Tensor prroi_pooling_backward_cuda(
     int bottom_count = batch_size * nr_channels * height * width;
 
     if (output.numel() == 0) {
-        THCudaCheck(cudaGetLastError());
+        AT_CUDA_CHECK(cudaGetLastError());
         return features_diff;
     }
 
@@ -71,7 +72,7 @@ at::Tensor prroi_pooling_backward_cuda(
         top_count, bottom_count
     );
 
-    THCudaCheck(cudaGetLastError());
+    AT_CUDA_CHECK(cudaGetLastError());
     return features_diff;
 }
 
@@ -89,7 +90,7 @@ at::Tensor prroi_pooling_coor_backward_cuda(
     int bottom_count = nr_rois * 5;
 
     if (output.numel() == 0) {
-        THCudaCheck(cudaGetLastError());
+        AT_CUDA_CHECK(cudaGetLastError());
         return coor_diff;
     }
 
@@ -102,7 +103,7 @@ at::Tensor prroi_pooling_coor_backward_cuda(
         top_count, bottom_count
     );
 
-    THCudaCheck(cudaGetLastError());
+    AT_CUDA_CHECK(cudaGetLastError());
     return coor_diff;
 }
 
